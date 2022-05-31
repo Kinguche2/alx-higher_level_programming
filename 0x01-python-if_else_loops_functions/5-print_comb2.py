5fda107f73634f38635d75aedcef783dafcbebc6
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-05-31 17:51:56 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("%02d, " % x, end="")
-print("\n")
+for i in range(0, 100):
+    if i == 99:
+        print(i)
+    else:
+        print("{:0>2d}".format(i), end=", ")
+        
