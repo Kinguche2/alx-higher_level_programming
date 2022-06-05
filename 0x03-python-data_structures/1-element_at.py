fe660234a643a1ad178d981895f7dd68afce4d9e
Uche Ogbonna <ucheogbonnak@gmail.com>
2022-06-05 11:01:27 -0700

    new

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     for x in range(len(my_list)):
         if idx == -idx:
-            return None
+            return 'None'
         elif idx > x:
-            return None
+            return 'None'
     else:
-        print("{}".format(my_list[idx]))
+        return my_list[idx]
