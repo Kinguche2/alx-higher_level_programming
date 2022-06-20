5cad67f20c5f6096e7c13eecb338c91491854da0
root <ucheogbonnak@gmail.com>
2022-06-20 12:42:29 -0700

    new

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        a / b
-    except BaseException:
-        return None
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
     finally:
-        print("Inside result:{}".format(a/b))
+        print("Inside result:{}".format(div))
+    return div
