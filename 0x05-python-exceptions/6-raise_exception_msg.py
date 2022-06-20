f819c669d7690c1dc809098d0dfd71c516a259ac
root <ucheogbonnak@gmail.com>
2022-06-20 13:41:07 -0700

    new

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def raise_exception():
-    ne = 'This is an error'
-    try:
-        exe = white
-        print("{:d}".format(exe))
-    except NameError:
-        print(ne)
+def raise_exception_msg(message=""):
+    raise NameError(message)
