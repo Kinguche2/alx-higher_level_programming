97f28721b1801036f316ea4723a3eff1d15db12e
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-06-28 02:20:13 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,21 +1,65 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+'''N Queens Problem'''
 
 
-from sys import argv
+def validation(chessboard, row, column):
+    '''validates current position to see if its available
+       vs the queens already set on the columns to the left.
+    Args:
+        chessboard: actual state of the game.
+        row: row to validate.
+        column: column to validate.
+    '''
+    for col in range(column):
+        # checks if there is no queen in the row or diagonal
+        if (chessboard[col] == row or
+                # checks the slope:
+                abs(col - column) == abs(chessboard[col] - row)):
+            return False
+    return True
+
+
+def backtracking(chessboard, column):
+    '''backtracking application.
+    Args:
+        chessboard: actual state of the game.
+        column: the colum to backtrack,
+    '''
+    q = len(chessboard)
+    # when all the queens are set and the validation is True,
+    # prints the solution
+    if column == q:
+        solution = []
+        for col in range(q):
+            solution.append([col, chessboard[col]])
+        print(solution)
+        return
+
+    for row in range(q):
+        # if validation is True, a new queen is set and start to test a new one
+        if validation(chessboard, row, column):
+            chessboard[column] = row
+            backtracking(chessboard, column + 1)
 
 if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
+    import sys
+
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
+        sys.exit(1)
+    q = 0
+    try:
+        q = int(sys.argv[1])
+    except BaseException:
         print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
+        sys.exit(1)
+    if q < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
+
+    # Creation of the chessboard
+    chessboard = []
+    for col in range(q):
+        chessboard.append(col)
+    # starts the seeking of the solution in the first column
+    backtracking(chessboard, 0)
