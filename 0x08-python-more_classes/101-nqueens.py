a99616cfcb5991338a647cc66dd7dee9ac43850c
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-06-28 02:15:48 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,19 +1,75 @@
 #!/usr/bin/python3
-""""Module to define a rectangle class"""
+"""This module contains an algorithm that resolves the N-Queen puzzle"""
 
 
-from sys import argv
+def isSafe(m_queen, nqueen):
+    """ Method that determines if the queens can or can't kill each other"""
 
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+    for i in range(nqueen):
 
-nqueens = argv[1]
+        if m_queen[i] == m_queen[nqueen]:
+            return False
 
-if not isinstance(nqueens, int):
-    print('N must be a number')
-    exit(1)
+        if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
+            return False
 
-if nqueens <= 4:
-    print('N must be at least 4')
-    exit(1)
+    return True
+
+
+def print_result(m_queen, nqueen):
+    """ Method that prints the list with the Queens positions"""
+
+    res = []
+
+    for i in range(nqueen):
+        res.append([i, m_queen[i]])
+
+    print(res)
+
+
+def Queen(m_queen, nqueen):
+    """ Recursive function that executes the Backtracking algorithm"""
+
+    if nqueen is len(m_queen):
+        print_result(m_queen, nqueen)
+        return
+
+    m_queen[nqueen] = -1
+
+    while((m_queen[nqueen] < len(m_queen) - 1)):
+
+        m_queen[nqueen] += 1
+
+        if isSafe(m_queen, nqueen) is True:
+
+            if nqueen is not len(m_queen):
+                Queen(m_queen, nqueen + 1)
+
+
+def solveNQueen(size):
+    """ Function that invokes the Backtracking algorithm"""
+
+    m_queen = [-1 for i in range(size)]
+
+    Queen(m_queen, 0)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        size = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+
+    if size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solveNQueen(size)
