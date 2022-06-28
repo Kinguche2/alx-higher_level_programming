3d24cd6aff8f55bb035f61d27a872296944a408e
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-06-28 02:10:11 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,123 +1,72 @@
 #!/usr/bin/python3
-""" defines a nqueens"""
-
-import sys
 
 
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return (board)
-
-
-def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return (board)
-
-
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return (solution)
-
-
-def xout(board, row, col):
-    """X out spots on a chessboard.
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
+class NQueens:
     """
-    # X out all forward spots
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    # X out all backwards spots
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    # X out all spots below
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    # X out all spots above
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    # X out all spots diagonally down to the right
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally up to the left
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    # X out all spots diagonally up to the right
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally down to the left
-    c = col - 1
-    for r in range(row + 1, len(board)):
-        if c < 0:
-            break
-        board[r][c] = "x"
-        c -= 1
-
-
-def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solutions (list): A list of lists of solutions.
-    Returns:
-        solutions
+    Class NQueens solves the problem nqueens by backtracking
+    Attributes:
+        attr1 (size): size of the grid
     """
-    if queens == len(board):
-        solutions.append(get_solution(board))
-        return (solutions)
+    def __init__(self, size):
+        """
+        Constructor of the class
+        Args:
+            param1 (size): size of the grid
+        """
+        self.size = size
+        self.solve()
 
-    for c in range(len(board)):
-        if board[row][c] == " ":
-            tmp_board = board_deepcopy(board)
-            tmp_board[row][c] = "Q"
-            xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
-                                        queens + 1, solutions)
+    def solve(self):
+        """
+        Function that initializes the class
+        """
+        positions = [-1] * self.size
+        self.put_queen(positions, 0)
 
-    return (solutions)
+    def put_queen(self, positions, row):
+        """
+        Function that puts the queen in a valid place
+        """
+        if row == self.size:
+            self.print_queen(positions)
+        else:
+            for col in range(self.size):
+                if self.check_queen(positions, row, col):
+                    positions[row] = col
+                    self.put_queen(positions, row + 1)
+
+    def check_queen(self, positions, row, col):
+        """
+        Function that checks if a given position is in a valid place
+        """
+        for i in range(row):
+            if positions[i] == col or \
+               positions[i] - i == col - row or \
+               positions[i] + i == col + row:
+                return False
+        return True
+
+    def print_queen(self, positions):
+        """
+        Function that prints the array of nqueen proble
+        """
+        print([[i, positions[i]] for i in range(self.size)])
 
 
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+
+    try:
+        n = int(sys.argv[1])
+    except BaseException:
         print("N must be a number")
         sys.exit(1)
+
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])
-    for sol in solutions:
-        print(sol)
+    NQueens(int(sys.argv[1]))
