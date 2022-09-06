65658b26ead701a47941778af4e446483e4bef89
root <ucheogbonnak@gmail.com>
2022-09-05 18:14:48 -0700

    new

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2])) {
+if (parseInt(process.argv[2]) === true) {
   console.log('My number:' + process.argv[2]);
 } else {
   console.log('Not a number');
