ac3e664335a09c605b636e433ce3d3e051352e59
root <ucheogbonnak@gmail.com>
2022-09-10 16:23:01 -0700

    fixed bug for second file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQL.connect(user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                        db=sys.argv[3], port=3306)
 
     curs = db.cursor()
