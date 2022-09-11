7bdbe06dd3c7073af745acceaa225c25b0fb12df
root <ucheogbonnak@gmail.com>
2022-09-10 17:58:33 -0700

    handled injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     curs.execute("SELECT * \
     FROM states \
     WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = %s';", (sys.argv[4]))
+    COLLATE Latin1_General_CS = %s;", (sys.argv[4],))
     states = curs.fetchall()
 
     for state in states:
