399d1391dfe14fa11a60a0c5836107c726a5e581
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-09-12 05:55:20 +0100

    Update relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,7 +6,7 @@ from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-
+if __name__ == '__main__':
 class City(Base):
     """
     Class that defines each city
