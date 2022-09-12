5dd354eef4b513981815f42c503352b19bdb699d
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-09-12 06:02:33 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import relationship
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
-if __name__ == '__main__':
 class State(Base):
     """
     Class with id and name attributes of each state
