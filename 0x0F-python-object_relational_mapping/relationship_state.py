3e8ed4cb4ebdd85df74f06a970992ac4993015d4
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-09-16 23:23:43 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""
-Contains State class and Base, an instance of declarative_base()
-"""
-from sqlalchemy import Column, Integer, String, MetaData
+"""Write a python file that contains the class definition of a
+State and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+
+Base = declarative_base()
+
 
 class State(Base):
-    """
-    Class with id and name attributes of each state
+    """[summary]
+    Args:
+        Base ([type]): [description]
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=True)
+    name = Column(String(128))
+    cities = relationship('City')
