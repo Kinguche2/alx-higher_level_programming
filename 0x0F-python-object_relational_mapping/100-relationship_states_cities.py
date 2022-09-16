9bbe54925f3d3d8479930454df7dea917d58e0e9
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-09-17 00:56:29 +0100

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""adds rows using relationship
 """
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
-"""
-
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
@@ -11,22 +8,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+    engine = create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-    cal_state = State(name='California')
-    sfr_city = City(name='San Francisco')
-    cal_state.cities.append(sfr_city)
-
-    session.add(cal_state)
+    session = sessionmaker(bind=engine)()
+    c = City(name="San Francisco")
+    s = State(name="California", cities=[c])
+    session.add(s)
+    session.add(c)
     session.commit()
-    session.close()
